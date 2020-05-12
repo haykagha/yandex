a01cd94846a525dccf515435896abcc606d5fba0
haykagha <avaghavelyan@gmail.com>
2020-05-12 23:56:07 +0400

    binary search

---
+++ binarySearch.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var searchElementInThisArray = [1,2,3,4,5,6,7,8,9]
+
+func searchElement(array: [Int],element:Int)-> Int {
+    var index: Int = 0
+    var l = -1
+    var r = array.count
+    var m = 0
+    while (r > l + 1) {
+        m = (r+l)/2
+        if array[m] >= element {
+            r = m
+        } else {
+            l = m
+        }
+    }
+    if r < array.count && array[r] == element {
+        index = r
+    } else {
+        index = -1
+    }
+    return index
+}
+
+searchElement(array: searchElementInThisArray, element: 9)
